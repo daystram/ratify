a193e4641b0fcb5e196a1da3ce985bb25b600413
Danny August Ramaputra <daystram@gmail.com>
2020-12-22 18:44:42 +1000

    Added user foreign key and ApplicationUpdate implementation

--- ratify-be/models/application.go
+++ ratify-be/models/application.go
@@ -11,6 +11,8 @@ type applicationOrm struct {
 // CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 type Application struct {
 	ID           string `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()" json:"-"`
+	Owner        User   `gorm:"foreignKey:OwnerSubject;references:Subject;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	OwnerSubject string
 	ClientID     string `gorm:"column:client_id;uniqueIndex;type:char(32);not null" json:"-"`
 	ClientSecret string `gorm:"column:client_secret;type:char(64);not null" json:"-"`
 	Name         string `gorm:"column:name;type:varchar(50);not null" json:"-"`
@@ -26,6 +28,7 @@ type Application struct {
 type ApplicationOrmer interface {
 	GetOneByClientID(clientID string) (application Application, err error)
 	InsertApplication(application Application) (clientID string, err error)
+	UpdateApplication(application Application) (err error)
 }
 
 func NewApplicationOrmer(db *gorm.DB) ApplicationOrmer {
@@ -34,12 +37,16 @@ func NewApplicationOrmer(db *gorm.DB) ApplicationOrmer {
 }
 
 func (o *applicationOrm) GetOneByClientID(clientID string) (application Application, err error) {
-	application.ClientID = clientID
-	result := o.db.Model(&Application{}).First(&application)
+	result := o.db.Model(&Application{}).Where("client_id = ?", clientID).First(&application)
 	return application, result.Error
 }
 
 func (o *applicationOrm) InsertApplication(application Application) (clientID string, err error) {
 	result := o.db.Model(&Application{}).Create(&application)
 	return application.ClientID, result.Error
+}
+
+func (o *applicationOrm) UpdateApplication(application Application) (err error) {
+	result := o.db.Model(&User{}).Where("client_id = ?", application.ClientID).Updates(&application)
+	return result.Error
 }
