dd15e374a8d0b645a8cba1c67a65ba5f29b58754
Danny August Ramaputra <daystram@gmail.com>
2020-12-30 01:46:00 +1000

    Added global styles

--- ratify-fe/src/main.ts
+++ ratify-fe/src/main.ts
@@ -7,6 +7,8 @@ import "./plugins/vuelidate";
 import vuetify from "./plugins/vuetify";
 import { StatusMixin } from "@/constants/status";
 
+import "@/styles/App.sass";
+
 Vue.config.productionTip = false;
 Vue.mixin(StatusMixin);
 new Vue({
