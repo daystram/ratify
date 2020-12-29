05fec7a31d857674eef0a9ef07d6a19164466144
Danny August Ramaputra <daystram@gmail.com>
2020-12-30 01:49:00 +1000

    Added auth helper functions

---
+++ ratify-fe/src/auth/index.ts
@@ -0,0 +1,72 @@
+import router from "@/router";
+import { AuthManager, KEY_ACCESS_TOKEN } from "@/auth/AuthManager";
+
+const CLIENT_ID = process.env.VUE_APP_CLIENT_ID;
+const ISSUER = process.env.VUE_APP_OAUTH_ISSUER;
+const REDIRECT_URI = `${location.origin}/callback`;
+
+const authManager = new AuthManager({
+  clientId: CLIENT_ID,
+  redirectUri: REDIRECT_URI,
+  issuer: ISSUER
+});
+
+const login = function() {
+  authManager.authorize();
+};
+
+const logout = function() {
+  authManager.revokeToken();
+  authManager.reset();
+  router.replace({ name: "home" });
+};
+
+const callback = function() {
+  const params = new URLSearchParams(document.location.search);
+  const code = params.get("code");
+  const state = params.get("state");
+  if (!code || !state || !authManager.checkState(state)) {
+    router.replace("/");
+    return;
+  }
+  authManager
+    .redeemToken(code)
+    .then(response => {
+      router.replace({ name: "manage" });
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(error.response.data);
+      router.replace({ name: "home" });
+    });
+};
+
+const isAuthenticated = function(): boolean {
+  return authManager.getToken(KEY_ACCESS_TOKEN) !== "";
+};
+
+const authenticatedOnly = function(to: any, from: any, next: () => void) {
+  if (authManager.getToken(KEY_ACCESS_TOKEN)) {
+    next();
+  } else {
+    authManager.reset();
+    router.push({ name: "login" });
+  }
+};
+
+const unAuthenticatedOnly = function(to: any, from: any, next: () => void) {
+  if (!authManager.getToken(KEY_ACCESS_TOKEN)) {
+    next();
+  } else {
+    router.push({ name: "manage" });
+  }
+};
+
+export {
+  isAuthenticated,
+  login,
+  logout,
+  callback,
+  authenticatedOnly,
+  unAuthenticatedOnly
+};
