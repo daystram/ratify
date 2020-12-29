f9288a0c251d28dcadc32290a0cd8fdb009e6f1d
Danny August Ramaputra <daystram@gmail.com>
2020-12-30 01:49:49 +1000

    Added theme configuration

--- ratify-fe/src/plugins/vuetify.ts
+++ ratify-fe/src/plugins/vuetify.ts
@@ -1,6 +1,23 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
+import "@mdi/font/css/materialdesignicons.css";
+
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: "#00c3c3",
+        primaryDim: "#008686",
+        secondary: "#c1842f"
+      }
+    },
+    options: { customProperties: true }
+  },
+  icons: {
+    iconfont: "mdi"
+  }
+});
