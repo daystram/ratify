a60f1338cc91ae9f4b577f3f964dbe5bc98af2c9
Danny August Ramaputra <daystram@gmail.com>
2020-12-30 01:56:00 +1000

    Bundled view exports

---
+++ ratify-fe/src/views/index.ts
@@ -0,0 +1,7 @@
+import Authorize from "./Authorize.vue";
+import Home from "./Home.vue";
+import Manage from "./Manage.vue";
+import Profile from "./Profile.vue";
+import User from "./User.vue";
+
+export { Authorize, Home, Manage, Profile, User };
