6f1df7d452577ac515cf6a62b90c685455d19720
Danny August Ramaputra <daystram@gmail.com>
2020-12-30 01:46:48 +1000

    Added token request oauth api for PKCE flow

--- ratify-fe/src/apis/oauth.ts
+++ ratify-fe/src/apis/oauth.ts
@@ -1,11 +1,22 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
+import qs from "qs";
 
 const oauthClient: AxiosInstance = axios.create({
   baseURL: "oauth/"
 });
 
 export default {
-  authorize: function(authRequest: any): Promise<AxiosResponse> {
+  authorize: function(authRequest: unknown): Promise<AxiosResponse> {
     return oauthClient.post(`authorize`, authRequest);
+  },
+  token: function(tokenRequest: unknown): Promise<AxiosResponse> {
+    return oauthClient.post(`token`, qs.stringify(tokenRequest), {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    });
+  },
+  revoke: function(revokeRequest: unknown): Promise<AxiosResponse> {
+    return oauthClient.post(`revoke`, revokeRequest);
   }
 };
