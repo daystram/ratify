31e7becf8f307032fd8abaf9877bffe325951dcf
Danny August Ramaputra <daystram@gmail.com>
2020-12-31 05:27:15 +1000

    Added OID claims struct, added scope field to authRequest

--- ratify-be/datatransfers/auth.go
+++ ratify-be/datatransfers/auth.go
@@ -5,11 +5,41 @@ import (
 	"time"
 )
 
+type OpenIDClaims struct {
+	Subject   string `json:"sub"`
+	Issuer    string `json:"iss"`
+	Audience  string `json:"aud"`
+	ExpiresAt int64  `json:"exp"`
+	IssuedAt  int64  `json:"iat"`
+
+	// scope: profile
+	Superuser  *bool   `json:"is_superuser,omitempty"`
+	Username   *string `json:"preferred_username,omitempty"`
+	GivenName  *string `json:"given_name,omitempty"`
+	FamilyName *string `json:"family_name,omitempty"`
+	UpdatedAt  *int64  `json:"updated_at,omitempty"`
+
+	// scope: email
+	Email         *string `json:"email,omitempty"`
+	EmailVerified *bool   `json:"email_verified,omitempty"`
+}
+
+func (c OpenIDClaims) Valid() (err error) {
+	now := time.Now()
+	if now.After(time.Unix(c.ExpiresAt, 0)) {
+		err = errors.New("token has expired")
+	}
+	if now.Before(time.Unix(c.IssuedAt, 0)) {
+		err = errors.New("token used before issued")
+	}
+	return err
+}
+
 type JWTClaims struct {
-	Subject     string `json:"sub,omitempty"`
-	IsSuperuser bool   `json:"sup,omitempty"`
-	ExpiresAt   int64  `json:"exp,omitempty"`
-	IssuedAt    int64  `json:"iat,omitempty"`
+	Subject     string `json:"sub"`
+	IsSuperuser bool   `json:"sup"`
+	ExpiresAt   int64  `json:"exp"`
+	IssuedAt    int64  `json:"iat"`
 }
 
 func (c JWTClaims) Valid() (err error) {
--- ratify-be/datatransfers/oauth.go
+++ ratify-be/datatransfers/oauth.go
@@ -8,6 +8,7 @@ type AuthorizationRequest struct {
 	ResponseType string `json:"response_type" binding:"required"`
 	ClientID     string `json:"client_id" binding:"required"`
 	RedirectURI  string `json:"redirect_uri" binding:"-"`
+	Scope        string `json:"scope" binding:"-"`
 	State        string `json:"state" binding:"-"`
 	UserLogin
 	PKCEAuthFields
