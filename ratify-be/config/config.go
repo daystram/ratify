161fdbb41715044cdb0d75b6fc9489f7cbe5aa77
Danny August Ramaputra <daystram@gmail.com>
2021-01-10 13:47:48 +1000

    Added SMTP configs

--- ratify-be/config/config.go
+++ ratify-be/config/config.go
@@ -25,6 +25,11 @@ type Config struct {
 	RedisPassword string
 	RedisDatabase int
 
+	SMTPServer   string
+	SMTPPort     int
+	SMTPUsername string
+	SMTPPassword string
+
 	Domain    string
 	JWTSecret string
 }
@@ -40,55 +45,34 @@ func InitializeAppConfig() {
 		log.Fatalf("[INIT] Unable to load configuration. %+v\n", err)
 	}
 
-	// Hostname
 	if AppConfig.Hostname = viper.GetString("hostname"); AppConfig.Hostname == "" {
 		log.Fatalln("[INIT] hostname is missing in config.yaml")
 	}
-
-	// Port
 	AppConfig.Port = viper.GetInt("port")
-
-	// Environment
 	if AppConfig.Environment = viper.GetString("environment"); AppConfig.Environment == "" {
 		log.Fatalln("[INIT] environment is missing in config.yaml")
 	}
-
-	// Debug
 	AppConfig.Debug = viper.GetBool("debug")
 
-	// DBHostname
 	AppConfig.DBHostname = viper.GetString("db_hostname")
-
-	// DBPort
 	AppConfig.DBPort = viper.GetInt("db_port")
-
-	// DBDatabase
 	AppConfig.DBDatabase = viper.GetString("db_database")
-
-	// DBUsername
 	AppConfig.DBUsername = viper.GetString("db_username")
-
-	// DBPassword
 	AppConfig.DBPassword = viper.GetString("db_password")
 
-	// RedisHostname
 	AppConfig.RedisHostname = viper.GetString("redis_hostname")
-
-	// RedisPort
 	AppConfig.RedisPort = viper.GetInt("redis_port")
-
-	// RedisPassword
 	AppConfig.RedisPassword = viper.GetString("redis_password")
-
-	// RedisDatabase
 	AppConfig.RedisDatabase = viper.GetInt("redis_database")
 
-	// Domain
+	AppConfig.SMTPServer = viper.GetString("smtp_server")
+	AppConfig.SMTPPort = viper.GetInt("smtp_port")
+	AppConfig.SMTPUsername = viper.GetString("smtp_username")
+	AppConfig.SMTPPassword = viper.GetString("smtp_password")
+
 	if AppConfig.Domain = viper.GetString("domain"); AppConfig.Domain == "" {
 		log.Fatalln("[INIT] domain is missing in config.yaml")
 	}
-
-	// JWTSecret
 	if AppConfig.JWTSecret = viper.GetString("secret"); AppConfig.JWTSecret == "" {
 		log.Fatalln("[INIT] secret is missing in config.yaml")
 	}
