fb9a9d3abbc9cbdebc330f1496ea1dac13b9296e
Danny August Ramaputra <daystram@gmail.com>
2021-01-13 03:00:31 +1000

    Added log constants

---
+++ ratify-be/constants/log.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	LogTypeLogin       = "LOGN"
+	LogTypeApplication = "APPN"
+
+	LogSeverityFatal = "F"
+	LogSeverityError = "E"
+	LogSeverityWarn  = "W"
+	LogSeverityInfo  = "I"
+)
